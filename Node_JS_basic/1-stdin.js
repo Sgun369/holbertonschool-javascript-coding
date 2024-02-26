3ef1ebd7073895e2054a96c04cd91e312b051445
Sgun369 <souhaiebjebali@gmail.com>
2024-02-26 12:18:41 +0100

    1-stdin.js

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+process.stdout.write('Welcome to Holberton School, What is your name?\n');
+
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+
+  if (name) {
+    process.stdout.write(`Your name is: ${name}\n`);
+  }
+  process.stdout.write('This important software is now closing\n');
+  process.exit();
+});
+
+process.on('exit', () => {
+  process.stdout.write('\n');
+});
