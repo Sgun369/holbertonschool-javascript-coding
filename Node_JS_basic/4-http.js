4262791cc515bac06acf631096b4959d02516505
Sgun369 <souhaiebjebali@gmail.com>
2024-03-03 11:36:32 +0100

    4-http.js

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.end('Hello Holberton School!\n');
+  res.writeHead(200, { 'Content-type': 'text/plain' });
+  res.end('Hello Holberton School!');
 });
 app.listen(1245);
 module.exports = app;
